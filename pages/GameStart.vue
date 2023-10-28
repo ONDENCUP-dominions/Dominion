dc15517e049bae334adc007856edafa7e8fb916d
nakashimashin <nakashimashin0517@gmail.com>
2023-10-28 18:53:11 +0900

    GameStartページ作成(#shin1)

--- pages/GameStart.vue
+++ pages/GameStart.vue
@@ -1,6 +1,24 @@
+<script setup lang="ts">
+</script>
+
 <template>
-    <div>
-      game start page
+    <div class="flex-content">
+      <Button class="start-button">Start</Button>
+      <Button class="top-button">Top</Button>
     </div>
-  </template>
+</template>
+
+<style scoped>
+.flex-content{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.start-button{
+  margin-top: 63%;
+}
+.top-button{
+  margin-top: 10%;
+}
+</style>
   
