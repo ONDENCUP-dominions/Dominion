f368193c31834f37e37562eb5c25689f5359d257
nakashimashin <nakashimashin0517@gmail.com>
2023-10-28 17:33:16 +0900

    store作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/store/counter";
+import { storeToRefs } from "pinia";
+const counterStore = useCounterStore();
+const { increment } = counterStore;
+const { count } = storeToRefs(counterStore);
+</script>
+
 <template>
     <div>
       ナンジャもんじゃ
+      <div>Count:{{ count }}</div>
+      <button @click="increment">たすよ</button>
+      <br />
       <NuxtLink to="/game">Play</NuxtLink>
     </div>
-  </template>
+</template>
+
+<style scoped>
+</style>
   
