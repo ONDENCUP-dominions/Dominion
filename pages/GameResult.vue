8f3e17661d6be83441716994014efe9ea452caa2
nakashimashin <nakashimashin0517@gmail.com>
2023-10-28 21:38:53 +0900

    resultページ作成(#shin1)

--- pages/GameResult.vue
+++ pages/GameResult.vue
@@ -1,6 +1,41 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/store';
+const store =  useCounterStore();
+</script>
+
+
 <template>
-    <div>
-      game result page
+    <div class="flex-content">
+      <div class="score1">1P:{{ store.count1 }}枚</div>
+      <div class="score">2P:{{ store.count2 }}枚</div>
+      <div class="score">3P:{{ store.count3 }}枚</div>
+      <div class="score">4P:{{ store.count4 }}枚</div>
+      <Button class="again-button">Play Again</Button>
+      <Button class="finish-button">Finish</Button>
     </div>
-  </template>
+</template>
+
+<style scoped>
+.flex-content{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.score{
+  font-size: 40px;
+  margin-top: 25px;
+  font-weight: 700;
+}
+.score1{
+  font-size: 40px;
+  margin-top: 100px;
+  font-weight: 700;
+}
+.again-button{
+  margin-top: 50px;
+}
+.finish-button{
+  margin-top: 40px;
+}
+</style>
   
