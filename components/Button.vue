6dc1d5a77305678cf9c3d71dd664437a6fe25465
nakashimashin <nakashimashin0517@gmail.com>
2023-10-28 18:38:27 +0900

    buttonコンポーネントのスタイリング(#shin1)

--- components/Button.vue
+++ components/Button.vue
@@ -9,6 +9,17 @@
 
 <style scoped>
 .button{
-    background-color: red;
+    background-color: #63F87B;
+    width: 200px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border: 1px solid #000000;
+    border-radius: 0.25rem;
+    font-weight: 700;
+    font-size: 200%;
 }
+.button:hover{
+    opacity: 0.7;
+}
+
 </style>
